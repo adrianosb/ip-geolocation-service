8dc613da346dee83dcd2a59f1fd54dcfae6b64bb
Adriano de Souza Barbosa <adrianosb@gmail.com>
2026-04-11 15:04:16 -0300

    feat(application): initialize ip geolocation service with basic structure
    Added initial application files including main application class,
    configuration, and test cases to set up the ip geolocation service.

---
+++ src/main/java/com/adriano/ip_geolocation_service/IpGeolocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adriano.ip_geolocation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpGeolocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpGeolocationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriano/ip_geolocation_service/IpGeolocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.ip_geolocation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpGeolocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
